c327c7d3ae648727a83f71d2fc24dddcb1ce8e41
Aran Mulholland <aranmulholland@gmail.com>
2013-01-28 11:31:55 +1100

    Randomised the question array

--- SchoolSurvey.Web/Scripts/Modules/SurveyModule.js
+++ SchoolSurvey.Web/Scripts/Modules/SurveyModule.js
@@ -6,60 +6,77 @@ define("SurveyModule", [
       "loadKoTemplate!Survey/surveyTemplate"], function (core, questionFactory) {
    core.register("SurveyModule", function (sandbox) {
 
-      var ko = sandbox.getObservable(),
-          
-          templateName = ko.observable("surveyTemplate"),
-          cssClassNameArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
-          title = ko.observable(''),
-          description = ko.observable(''),
-          surveyQuestions = ko.observableArray(),
-          viewModel = {
-             templateName: templateName,
-             title: title,
-             description: description,
-             surveyQuestions: surveyQuestions
-          },
-          totalSpend = 100,
-          adjustSliders = function (itemNumber, amountToReduceBy) {
-             var surveyQuestionArray = surveyQuestions(),
-                 i,
-                 arrayLength = surveyQuestionArray.length,
-                 currentItem,
-                 currentItemValue;
+      var ko = sandbox.getObservable(),     
+      templateName = ko.observable("surveyTemplate"),
+      cssClassNameArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
+      title = ko.observable(''),
+      description = ko.observable(''),
+      surveyQuestions = ko.observableArray(),
+      totalSpent = ko.observable(0),
+      viewModel = {
+         templateName: templateName,
+         title: title,
+         description: description,
+         surveyQuestions: surveyQuestions,
+         totalSpent: totalSpent
+      },
+      totalSpend = 100,
+      adjustSliders = function (itemNumber, amountToReduceBy) {
+         var surveyQuestionArray = surveyQuestions(),
+            i,
+            arrayLength = surveyQuestionArray.length,
+            currentItem,
+            currentItemValue;
              
-             while(amountToReduceBy > 0) {
-                for(i = 0; i < arrayLength && amountToReduceBy > 0; i++) {
-                   currentItem = surveyQuestionArray[i];
-                   if (currentItem.questionNumber !== itemNumber) {
-                      currentItemValue = currentItem.sliderStuff.currentValue();
-                      if (currentItemValue > 0) {
-                         currentItem.sliderStuff.currentValue(currentItemValue - 1);
-                         amountToReduceBy = amountToReduceBy - 1;
-                      }
-                   }
-                }
-             } 
-          },
-          valueChanged = function (itemNumber, newValue) {
-             var total = surveyQuestions().map(function(item) {
-                return item.sliderStuff.currentValue();
-             }).integerSum();
+         while(amountToReduceBy > 0) {
+            for(i = 0; i < arrayLength && amountToReduceBy > 0; i++) {
+               currentItem = surveyQuestionArray[i];
+               if (currentItem.questionNumber !== itemNumber) {
+                  currentItemValue = currentItem.sliderStuff.currentValue();
+                  if (currentItemValue > 0) {
+                     currentItem.sliderStuff.currentValue(currentItemValue - 1);
+                     amountToReduceBy = amountToReduceBy - 1;
+                  }
+               }
+            }
+         } 
+      },
+      valueChanged = function (itemNumber) {
+         var total = surveyQuestions().map(function(item) {
+            return item.sliderStuff.currentValue();
+         }).integerSum();
                 
-             if (total > totalSpend) {
-                adjustSliders(itemNumber, (total - totalSpend));
-             }
-          },
-          loadQuestions = function () {
-             totalSpend = 100;
-             title('Success of the school');
-             description('In this question you are going to build a successful school. You have 100 success dollars to spend.');
-             surveyQuestions.push(questionFactory.create(sandbox, 'one', 'Academic performance', 0, 100, valueChanged));
-             surveyQuestions.push(questionFactory.create(sandbox, 'two', 'Spiritual development', 0, 100, valueChanged));
-             surveyQuestions.push(questionFactory.create(sandbox, 'three', 'Positive relationships between students', 0, 100, valueChanged));
-             surveyQuestions.push(questionFactory.create(sandbox, 'four', 'Positive relationships between students and teachers', 0, 100, valueChanged));
-             surveyQuestions.push(questionFactory.create(sandbox, 'five', 'Number of subjects offered', 0, 100, valueChanged));
-             surveyQuestions.push(questionFactory.create(sandbox, 'six', 'Community involvement', 0, 100, valueChanged));
-          };
+         if (total > totalSpend) {
+            adjustSliders(itemNumber, (total - totalSpend));
+            totalSpent(100);
+         }
+         else {
+            totalSpent(totalSpend);
+         }
+      },
+      randomiseArray =function ( myArray ) {
+         var i = myArray.length, j, tempi, tempj;
+         if ( i == 0 ) return false;
+         while ( --i ) {
+            j = Math.floor( Math.random() * ( i + 1 ) );
+            tempi = myArray[i];
+            tempj = myArray[j];
+            myArray[i] = tempj;
+            myArray[j] = tempi;
+         }
+      },
+      loadQuestions = function () {
+         totalSpend = 100;
+         title('Success of the school');
+         description('In this question you are going to build a successful school. You have 100 success dollars to spend.');
+
+         var questions = ['Academic performance', 'Spiritual development', 'Positive relationships between students', 'Positive relationships between students and teachers', 'Number of subjects offered', 'Community involvement'];
+         randomiseArray(questions);
+
+         for (var i = 0; i < questions.length; i++) {
+            surveyQuestions.push(questionFactory.create(sandbox, cssClassNameArray[i], questions[i], 0, 100, valueChanged));
+         }
+      };
 
       return {
          activate: function () {
